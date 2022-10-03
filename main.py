856d108dc4ac2ac85485571fcfe58f0e7e7d5628
Basith P <basithpofficial@gmail.com>
2022-10-03 10:32:32 +0530

    Display number of correct guesses on title

--- main.py
+++ main.py
@@ -14,17 +14,17 @@ guessed_states = []
 game_is_on = True
 while len(guessed_states) < 50 and game_is_on:
     answer_state = screen.textinput(
-        title="Guess the State", prompt="What's another state's name?")
+        title=f"{len(guessed_states)}/50 are correct", prompt="What's another state's name?").title()
 
     if answer_state == "Exit":
         game_is_on = False
-    elif answer_state.title() in all_states:
+    elif answer_state in all_states:
         t = turtle.Turtle()
         t.hideturtle()
         t.penup()
-        state_data = data[data.state == answer_state.title()]
+        state_data = data[data.state == answer_state]
         t.goto(int(state_data.x), int(state_data.y))
-        t.write(answer_state.title())
-        guessed_states.append(answer_state.title())
+        t.write(answer_state)
+        guessed_states.append(answer_state)
     else:
         print("Not a state")
